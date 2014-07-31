9216f01dc5d21a98eafacbf0afa517ca079456a0
Near <nearsy.h@gmail.com>
2014-07-31 14:52:36 -0400

    add the controls files; set it in the index.js

--- index.js
+++ index.js
@@ -2,33 +2,71 @@
 
 var breach = require('breach_module');
 var common = require('./lib/common.js');
+var http = require('http')
 var async = require('async');
+var express = require('express');
 
 common._ = {
   vim_handler: require('./lib/vim_handler.js').vim_handler({})
 }
 
-breach.init(function() {
-  breach.register('core', 'inst:.*');
-  breach.register('core', 'tabs:keyboard');
-  breach.register('core', 'controls:keyboard');
+var bootstrap = function(http_srv) {
+  var http_port = http_srv.address().port;
+  breach.init(function() {
+    breach.register('core', 'inst:.*');
+    breach.register('core', 'tabs:keyboard');
+    breach.register('core', 'controls:keyboard');
 
-  breach.expose('init', function(src, args, cb_) {
-    console.log('Vimmode Init');
-    async.parallel([
-      common._.vim_handler.init
-    ], cb_);
-  });
+    breach.expose('init', function(src, args, cb_) {
+      console.log('Vimmode Init');
+      breach.module('core').call('controls_set', {
+        type: 'TOP',
+        url: 'http://127.0.0.1:' + http_port + '/inputline',
+        dimension: 200,
+      }, cb_);
+      async.parallel([
+        common._.vim_handler.init
+        ], cb_);
+    });
 
-  breach.expose('kill', function(args, cb_) {
-    console.log('Vimmode Exit');
-    async.parallel([
-      common._.vim_handler.kill
-    ], function(err) {
-      common.exit(0);
+    breach.expose('kill', function(args, cb_) {
+      console.log('Vimmode Exit');
+      breach.module('core').call('controls_unset', {
+        type: 'BOTTOM',
+      }, cb_);
+      async.parallel([
+        common._.vim_handler.kill
+        ], function(err) {
+          common.exit(0);
+        });
     });
   });
-});
+};
+
+(function setup() {
+  var app = express();
+
+  var args = process.argv;
+  args.forEach(function(a) {
+    if(a === '--debug') {
+      common.DEBUG = true;
+    }
+  });
+
+  /* App Configuration */
+  app.use('/', express.static(__dirname + '/controls'));
+  app.use(require('body-parser')());
+  app.use(require('method-override')());
+
+  /* Listen locally only */
+  var http_srv = http.createServer(app).listen(0, '127.0.0.1');
+
+  http_srv.on('listening', function() {
+    var port = http_srv.address().port;
+    console.log('HTTP Server started on `http://127.0.0.1:' + port + '`');
+    return bootstrap(http_srv);
+  });
+})();
 
 process.on('uncaughtException', function(err) {
   common.fatal(err);
